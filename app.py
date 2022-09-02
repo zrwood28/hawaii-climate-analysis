ce818775cb443a27f2bd68470d92cab72aee409f
Zach Wood <zachrwood28@gmail.com>
2022-09-02 14:48:47 -0500

    Added flask file and updated queries.

---
+++ app.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect = True)
+
+Station = Base.classes.station
+Measurement = Base.classes.measurement
+
+session = Session(engine)
